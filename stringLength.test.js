10b68dca48d1ee670d0ec66a3e63e4ffe8d30cb6
Odongo006 <www.henryodongo006@gmail.com>
2023-02-02 12:12:53 +0300

    Complete exercise

---
+++ stringLength.test.js
@@ -0,0 +1,17 @@
+const stringFunctions = require('./stringLength');
+
+test('test correct string length', () => {
+  expect(stringFunctions.stringLength('Hello')).toBe(5);
+});
+
+test('test incorrect string length', () => {
+  expect(stringFunctions.stringLength('')).toBe('string length error');
+});
+
+test('test incorrect string length', () => {
+  expect(stringFunctions.stringLength('Hello world!')).toBe('string length error');
+});
+
+test('test correct string reverse', () => {
+  expect(stringFunctions.reverseString('Hello')).toBe('olleH');
+});
